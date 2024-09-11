414f3d301f58871361c0ed9ce2f2942b5169f1fd
ELVIN_LAPTOP\elvin_hs21rsf <Elving574@gmail.com>
2024-09-10 21:23:57 -0400

    fix equal case

--- BinaryTree/BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree/BinaryTree.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
-
+enum TreeOps {
+    AddValue = 1,
+    InOrder,
+    PreOrder,
+    PostOrder,
+    Exit
+};
 struct Node {
     int data;
     Node* left, * right;
@@ -30,6 +36,28 @@ public:
     
     }
 
+    void inOrder(Node* node) {
+        if (node != nullptr) {
+            inOrder(node->left);
+            cout << node->data << " ";
+            inOrder(node->right);
+        }
+    }
+    void preOrder(Node* node) {
+        if (node != nullptr) {
+            cout << node->data << " ";
+            preOrder(node->left);
+            preOrder(node->right);
+        }
+    }
+    void postOrder(Node* node) {
+        if (node != nullptr) {
+            postOrder(node->left);
+            postOrder(node->right);
+            cout << node->data << " ";
+        }
+    }
+
 private:
     Node* insert(Node* node, int value) {
         if (node == nullptr) {
@@ -41,45 +69,59 @@ private:
         else if (value > node->data) {
             node->right = insert(node->right, value);
         }
-        return node;
-    }
-
-    void inOrder(Node* node) {
-        if (node != nullptr) {
-            inOrder(node->left);
-            cout << node->data << " ";
-            inOrder(node->right);
+        else {
+            if (node->right == nullptr) {
+                node->right = insert(node->right, value);
+            }
+            else {
+                Node* oldRight = node->right;
+                node->right = insert(node->right, value);
+                node->right->right = oldRight;
+            }
         }
+        return node;
     }
 };
 
+
+
 int main() {
     BinaryTree tree;
-    int choice;
+    TreeOps choice;
     int value;
 
     do {
         cout << "1. add\n";
         cout << "2. view in order\n";
-        cout << "3. exit\n";
+        cout << "3. view pre order\n";
+        cout << "4. view post order\n";
+        cout << "5. exit\n";
         cout << "choice: ";
-        cin >> choice;
+        int ch;
+        cin >> ch;
+        choice = (TreeOps)ch;
 
         switch (choice) {
-        case 1:
+        case AddValue:
             cout << "value to add: ";
             cin >> value;
             tree.add(value);
             break;
-        case 2:
+        case InOrder:
             tree.sort();
             break;
-        case 3:
+        case PreOrder:
+            tree.preOrder(tree.root);
+            break;
+        case PostOrder:
+            tree.postOrder(tree.root);
+            break;
+        case Exit:
             cout << "exiting\n";
             break;
         default:
             cout << "invalid choice\n";
         }
-    } while (choice != 3);
+    } while (choice != Exit);
     return 0;
 }
